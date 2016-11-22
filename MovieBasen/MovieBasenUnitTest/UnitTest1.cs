71b46fc39927348d2282133f43feb8be91861ea9
mert1994 <ince@live.dk>
2016-11-22 15:37:15 +0100

    Adding Unit test again
    Bug fixes

---
+++ MovieBasen/MovieBasenUnitTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieBasen.Models;
+
+namespace MovieBasenUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Movie movie = new Movie();
+
+        [TestMethod]
+        public void TypeMovieNameFailTest()
+        {
+            movie.Name = "";
+            if (movie.Name == string.Empty)
+            {
+                throw new Exception("Movie name can't be empty");
+                //Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public void TypeMovieNameSucceed()
+        {
+            movie.Name = "Jack the Giantslayer";
+            if (movie.Name == string.Empty)
+            {
+                throw new Exception("Movie name can't be empty");
+                //Assert.Fail();
+            }
+
+<<<<<<< HEAD
+        }
+    }
+}
+=======
+
+        }
+    }
+>>>>>>> refs/remotes/origin/master
