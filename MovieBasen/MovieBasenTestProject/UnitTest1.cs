3e6ef83afe0533f282a1e2edb3514b4b96c37171
mert1994 <ince@live.dk>
2016-11-22 15:42:48 +0100

    First Unit Test added (Check if movie name is empty)
    Checks if the movie name is empty, if it is then fail the test.

---
+++ MovieBasen/MovieBasenTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieBasen.Models;
+
+namespace MovieBasenTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Movie movie = new Movie();
+
+        [TestMethod]
+        public void TypeMovieNameFailTest()
+        {
+            movie.Name = "";
+            if (movie.Name == string.Empty)
+            {
+                throw new Exception("Movie name can't be empty");
+                //Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public void TypeMovieNameSucceed()
+        {
+            movie.Name = "Jack the Giantslayer";
+            if (movie.Name == string.Empty)
+            {
+                throw new Exception("Movie name can't be empty");
+                //Assert.Fail();
+            }
+
+        }
+    }
+}
