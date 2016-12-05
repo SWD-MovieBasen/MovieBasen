918073eda2f8ad49c2c06a7c5f5c80adc0dbbd03
DAT14V1Elvis <elvi0042@stud.kea.dk>
2016-12-05 16:07:29 +0100

    Integration test

---
+++ MovieBasen/MovieBasen/Controllers/ConnectionTestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MovieBasen.Controllers
+{
+    public class ConnectionTestController : Controller
+    {
+        // GET: ConnectionTest
+        public ActionResult Index()
+        {
+            {
+                try
+                {
+                    SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                    connection.Open();
+                    if ((connection.State & ConnectionState.Open) == 0)
+                    {
+                        Response.Write("Connection OK!");
+                        connection.Close();
+                    }
+                    else
+                    {
+                        Response.Write("No Connection!");
+                    }
+                }
+                catch
+                {
+                    Response.Write("No Connection!");
+                }
+            }
+            return View();
+        }
+    }
+}
